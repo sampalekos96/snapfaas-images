e0cdcdaf7fb4c251b7b9b39775e5ed7fda52df1c
Yue Tan <yuetan@cs.princeton.edu>
2020-05-31 01:25:59 -0500

    update python2 wrapper to use do-snapshot

--- common/do-snapshot.c
+++ common/do-snapshot.c
@@ -5,7 +5,8 @@
 #include <sys/mount.h>
 #include <time.h>
 int main(int argc, char * argv[]) {
+  int val = strtol(argv[1], NULL, 10);
   if (iopl(3)) {perror("iopl"); exit(1);}
   outl(124, 0x3f0);
-  outl(126, 0x3f0);
+  outl(val, 0x3f0);
 }
--- separate/runtimes/python2/workload.py
+++ separate/runtimes/python2/workload.py
@@ -12,32 +12,22 @@ with open('/dev/ttyS1', 'r') as tty, open('/dev/ttyS1', 'w') as out:
     # for language snapshot
     for i in range(1, mp.cpu_count()):
         Popen('taskset -c %d outl 124 0x3f0'%(i), shell=True)
-    call('taskset -c 0 outl 124 0x3f0', shell=True)
-
-    call(['outl', '123', '0x3f0'], executable="/usr/bin/outl")
+    call('taskset -c 0 do-snapshot 123', shell=True)
 
     call(["mount", "-r", "/dev/vdb", "/srv"], executable="/bin/mount")
 
     sys.path.append('/srv/package')
     app = imp.load_source('app', '/srv/workload')
 
-    # signal firerunner we are ready
-    #call(['outl', '126', '0x3f0'], executable="/usr/bin/outl")
-    #call(['outl', '126', '0x3f0'], executable="/usr/bin/outl")
-
     # for function diff snapshot
     for i in range(1, mp.cpu_count()):
         Popen('taskset -c %d outl 124 0x3f0'%(i), shell=True)
-    call('taskset -c 0 outl 124 0x3f0', shell=True)
+    call('taskset -c 0 do-snapshot 126', shell=True)
 
-    call(['outl', '126', '0x3f0'], executable="/usr/bin/outl")
     while True:
         request = json.loads(tty.readline())
-        #t0 = time.clock()
 
         response = app.handle(request)
-        #t1 = time.clock()
-        #response['runtime'] = (t1-t0) * 1000
 
         responseJson = json.dumps(response)
 
