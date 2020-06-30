5fdf41e7a7606ecb326298839a2e64c01ec20212
Yue Tan <yuetan@cs.princeton.edu>
2020-06-29 22:37:28 -0500

    keep only one copy of dummy-carray for noop-carray-[0-10]m

--- appfs/noop-carray-5m/dummy-carray/setup.py
+++
@@ -1,7 +0,0 @@
-from distutils.core import setup, Extension
-
-module1 = Extension('dummy_array', sources = ['dummy.c'])
-
-setup (name = 'dummy_array', version = '1.0',
-        description = 'This package allocates and frees an dummy array', 
-        ext_modules = [module1])
