97ad7ac8bc08aa77940aa87709be5986a7a5c336
Yue Tan <yuetan@cs.princeton.edu>
2020-08-01 02:07:23 -0500

    move ioctl.c to common/ and call it before python3 launches to avoid hanging

--- separate/prelude.sh
+++ separate/prelude.sh
@@ -3,6 +3,7 @@ apk add openrc util-linux
 cp /common/outl /usr/bin/outl
 cp /common/do-snapshot /usr/bin/do-snapshot
 cp /common/setup-eth0.sh /usr/bin/setup-eth0.sh
+cp /common/ioctl /usr/bin/ioctl
 
 ## Create start script for that mounts the appfs and invokes whatever binary is in /srv/workload
 printf '#!/bin/sh\n
