7f843344545ee03711bc17a3870ba9c538d43197
Amit Aryeh Levy <amit@amitlevy.com>
2020-08-14 16:03:56 -0700

    Fix vsock for Node.js and modernize interface
    The Node vsock implementation was buggy when there were more than two
    requests or if the response was large enough to require multiple writes
    to the Vsock. This is because the vsock `write` was not actually
    preventing the read (which was blocking) from beginning. So we could get
    stuck in situations where the read had begun, and was blocking a write,
    and no more requests would arrive.
    
    This commit fixes the problem by acutally making the read/write requests
    non-blocking using Node's Readable and Writeable stream interface.
    
    Moreover, the commit changes the interface for Node.js apps to use the
    more contemporary Promise-style interface, which allows both the runtime
    and app to use `async/await` syntax for much simpler to reason about
    implementations.

--- separate/runtimes/nodejs/vsock/index.js
+++ separate/runtimes/nodejs/vsock/index.js
@@ -2,24 +2,69 @@ const fs = require('fs');
 
 const bindings = require('bindings')('vsock');
 
-exports.connect = bindings.connect;
+class Vsock {
+  constructor(fd) {
+    this.fd = fd;
+    this.readStream = fs.createReadStream("", { fd: fd });
+    this.writeStream = fs.createWriteStream("", { fd: fd });
+    this.readBuffer = Buffer.alloc(0);
+    this.readTarget = 0;
+    this.readEvent = null;
+
+    this.readStream.on('data', (chunk) => {
+      this.readBuffer = Buffer.concat([this.readBuffer, chunk]);
+      this.checkReadReady();
+    });
+  }
+
+  checkReadReady() {
+    if (this.readEvent && this.readBuffer.length >= this.readTarget) {
+      const resBuf = this.readBuffer.slice(0, this.readTarget);
+      this.readBuffer = this.readBuffer.slice(this.readTarget);
+      const evt = this.readEvent;
+      this.readEvent = null;
+      process.nextTick(() => evt(resBuf));
+    }
+  }
+
+  read(i) {
+    return new Promise((resolve, reject) => {
+      this.readEvent = resolve;
+      this.readTarget = i;
+      this.checkReadReady();
+    });
+  }
+
+  write(chunk) {
+    return new Promise((resolve, reject) => {
+      if (this.writeStream.write(chunk)) {
+        process.nextTick(resolve);
+      } else {
+        this.writeStream.once('drain', resolve);
+      }
+    });
+  }
+}
+
+exports.connect = function(port, cid) {
+  const fd = bindings.connect(port, cid);
+  return new Vsock(fd);
+}
 exports.read = bindings.read;
 
-exports.readRequest = function(fd) {
+exports.readRequest = async function(vs) {
   let len = 0;
-  let lenBuf = Buffer.alloc(4);
-  bindings.read(fd, lenBuf);
+  let lenBuf = await vs.read(4);
   len = lenBuf.readUInt32BE();
-  let requestBuf = Buffer.alloc(len);
-  bindings.read(fd, requestBuf);
+  let requestBuf = await vs.read(len);
   return JSON.parse(requestBuf);
 }
 
-exports.writeResponse = function(fd, resp) {
+exports.writeResponse = async function(vs, resp) {
   let respBuf = JSON.stringify(resp);
   let buf = Buffer.alloc(4 + respBuf.length);
   let offset = buf.writeUInt32BE(respBuf.length);
   buf.write(respBuf, offset);
-  fs.writeSync(fd, buf);
+  return await vs.write(buf);
 }
 
