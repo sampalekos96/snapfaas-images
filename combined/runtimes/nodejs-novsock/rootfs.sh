4bb7a7cb6f53531c53e51cb7244bf4511da35c6a
Yue Tan <yuetan@cs.princeton.edu>
2020-09-11 15:39:27 -0500

    add appfs/nodejs/hello and combined/runtimes/nodejs-novsock

---
+++ combined/runtimes/nodejs-novsock/rootfs.sh
@@ -0,0 +1,6 @@
+apk add nodejs
+
+cp /common/do-snapshot /usr/bin/do-snapshot
+cp /runtime/workload.js /bin/runtime-workload.js
+cp /runtime/workload.sh /bin/runtime-workload
+chmod +x /bin/runtime-workload
