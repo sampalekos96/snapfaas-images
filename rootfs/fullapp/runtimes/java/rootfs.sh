79aa396a112aaa8d47e81ff4998e1ed15ff31f48
lnqq <lnqq@node.lnqq-qv83303.praxis-pg0.wisc.cloudlab.us>
2020-11-04 23:49:23 -0600

    remove app placeholders for java regular and fullapp

--- rootfs/fullapp/runtimes/java/rootfs.sh
+++ rootfs/fullapp/runtimes/java/rootfs.sh
@@ -2,12 +2,12 @@ apk add bash openjdk8 build-base make linux-headers maven
 
 export JAVA_HOME=/usr/lib/jvm/java-1.8-openjdk
 
+# Copy application over
+cp -r /my-app/* /srv
+
 cd /runtime ; mvn clean package ; cd /
 cp /runtime/vsock-native/target/*.so /lib/
 cp /runtime/runtime/target/*dependencies.jar /bin/RuntimeWorkload.jar
-
-# Copy application over
-cp -r /my-app/* /srv
 
 cp /runtime/workload.sh /bin/runtime-workload
 chmod +x /bin/runtime-workload
