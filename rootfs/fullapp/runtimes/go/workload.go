5eafb70db630526f5a52a5c130a756327bbd57a3
Amit Aryeh Levy <amit@amitlevy.com>
2020-11-30 22:55:34 -0800

    Fullapp and Regular runtimes for Go

---
+++ rootfs/fullapp/runtimes/go/workload.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"encoding/binary"
+	"encoding/json"
+	"os/exec"
+	"runtime"
+
+	"golang.org/x/sys/unix"
+)
+
+func main() {
+	Init()
+
+	for i := 1; i < runtime.NumCPU(); i++ {
+		exec.Command("taskset", "-c", string(i), "do-snapshot", "123").Run()
+	}
+	exec.Command("taskset", "-c", "0", "do-snapshot", "123").Run()
+
+	fd, err := unix.Socket(unix.AF_VSOCK, unix.SOCK_STREAM, 0)
+	if err != nil {
+		panic(err)
+	}
+	sa := unix.SockaddrVM{
+		CID:  2,
+		Port: 1234,
+	}
+	err = unix.Connect(fd, &sa)
+	if err != nil {
+		panic(err)
+	}
+
+	for {
+		lenbuf := make([]byte, 4)
+		_, err = unix.Read(fd, lenbuf)
+		if err != nil {
+			panic(err)
+		}
+		reqlen := binary.BigEndian.Uint32(lenbuf)
+		reqbuf := make([]byte, reqlen)
+		_, err = unix.Read(fd, reqbuf)
+		if err != nil {
+			panic(err)
+		}
+
+		response := Handle(reqbuf)
+
+		responseBuf, err := json.Marshal(response)
+
+		binary.BigEndian.PutUint32(lenbuf, uint32(len(responseBuf)))
+		_, err = unix.Write(fd, lenbuf)
+		if err != nil {
+			panic(err)
+		}
+		unix.Write(fd, responseBuf)
+	}
+}
