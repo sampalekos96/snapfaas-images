b65a54ff04326fd530cfc62e5fcdf59d9c8dfa93
sampalekos96 <asabanis@cslab.ece.ntua.gr>
2022-10-27 17:06:35 +0000

    Changes needed for aarch64

--- rootfs/common/debug.sh
+++ rootfs/common/debug.sh
@@ -1,8 +1,8 @@
 ## Setup console
-ln -s agetty /etc/init.d/agetty.ttyS0
-echo ttyS0 > /etc/securetty
-rc-update add agetty.ttyS0 default
-rc-update add agetty.ttyS0 nonetwork
+ln -s agetty /etc/init.d/agetty.ttyS1
+echo ttyS1 > /etc/securetty
+rc-update add agetty.ttyS1 default
+rc-update add agetty.ttyS1 nonetwork
 
 echo agetty_options=\"-a root\" >> /etc/conf.d/agetty
 
--- rootfs/common/prelude.sh
+++ rootfs/common/prelude.sh
@@ -1,6 +1,6 @@
 apk add openrc util-linux build-base ca-certificates rng-tools rng-tools-openrc
 
-cp /common/outl /usr/bin/outl
+#cp /common/outl /usr/bin/outl
 cp /common/setup-eth0.sh /usr/bin/setup-eth0.sh
 cp /common/ioctl /usr/bin/ioctl
 
@@ -21,3 +21,12 @@ rc-update add serverless-workload default
 
 ## specify default system library path
 echo '/srv/lib:/lib:/usr/local/lib:/usr/lib' > /etc/ld-musl-$(uname -m).path
+
+## Setup console
+ln -s agetty /etc/init.d/agetty.ttyS0
+echo ttyS0 > /etc/securetty
+rc-update add agetty.ttyS0 default
+rc-update add agetty.ttyS0 nonetwork
+
+echo agetty_options=\"-a root\" >> /etc/conf.d/agetty
+
